337f4ab659dd897db7f54c1857a4451e7100682f
baesh3744 <baesh3744@naver.com>
2023-03-12 17:21:58 +0900

    feat: Provider 및 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC, Suspense, lazy } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+export enum PageType {
+  INDEX = '/',
+  LOGIN = '/login',
+  MANAGE = '/manage',
+}
+
+const routes = [
+  {
+    path: PageType.LOGIN,
+    element: lazy(() => import('./pages/Login')),
+  },
+  {
+    path: PageType.MANAGE,
+    element: lazy(() => import('./pages/Manage')),
+  },
+];
 
 const App: FC = () => {
-  return <div></div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={null}>
+          <Routes>
+            {routes.map((route) => (
+              <Route key={route.path} path={route.path} element={<route.element />} />
+            ))}
+            <Route path="*" element={<Navigate to={PageType.LOGIN} />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
