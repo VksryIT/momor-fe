0606180e1ee04c0d01664d031295d735c7b8ab02
baesh3744 <baesh3744@naver.com>
2023-05-21 16:19:45 +0900

    Page 공통 컴포넌트 추가

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,19 @@
+import GNB from 'components/GNB';
+import LNB from 'components/LNB';
+import React, { FC, ReactNode } from 'react';
+
+interface PageProps {
+  children: ReactNode;
+}
+
+const Page: FC<PageProps> = ({ children }) => {
+  return (
+    <>
+      <GNB />
+      <LNB />
+      <div className="contents">{children}</div>
+    </>
+  );
+};
+
+export default Page;
