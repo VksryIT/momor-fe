8caacfcd860ed4aea5dc3c91f69d135e5a3101cc
baesh3744 <baesh3744@naver.com>
2023-05-21 16:11:33 +0900

    API Response 인터페이스 수정

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,19 +1,28 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true;
 
-export interface APIResponse {
-  status: number;
-  message: string;
+interface ResponseData<T> {
+  data: T;
 }
 
+interface ErrorData {
+  code: string;
+}
+
+export type APIResponse<T> = AxiosResponse<ResponseData<T>>;
+export type APIError = AxiosError<ErrorData>;
+export type APIPromise<T = any> = Promise<APIResponse<T>>;
+
 abstract class API {
-  get(url: string, config?: AxiosRequestConfig) {
-    return axios.get(url, config);
+  async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response = await axios.get<ResponseData<T>>(url, config);
+    return response.data.data;
   }
 
-  post(url: string, data?: any, config?: AxiosRequestConfig) {
+  post(url: string, data?: any, config?: AxiosRequestConfig): APIPromise {
     return axios.post(url, data, config);
   }
 }
